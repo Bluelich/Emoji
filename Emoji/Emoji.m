8d4ff4220195593f871aff91b293f1273fb3c025
Bluelich <bluelich@qq.com>
2017-12-26 19:18:18 +0800

    update

--- Emoji/Emoji.m
+++ Emoji/Emoji.m
@@ -298,3 +298,52 @@ NSString *EmojiRegex(){
      */
     return @"";
 }
+NSString *arc4randomString(){
+    /*
+     字符集          字数     Unicode 编码
+     基本汉字      20902字    4E00-9FA5
+     基本汉字补充      38字    9FA6-9FCB
+     扩展A         6582字    3400-4DB5
+     扩展B        42711字    20000-2A6D6
+     扩展C         4149字    2A700-2B734
+     扩展D          222字    2B740-2B81D
+     康熙部首        214字    2F00-2FD5
+     部首扩展        115字    2E80-2EF3
+     兼容汉字        477字    F900-FAD9
+     兼容扩展        542字    2F800-2FA1D
+     PUA(GBK)部件    81字    E815-E86F
+     部件扩展        452字    E400-E5E8
+     PUA增补        207字    E600-E6CF
+     汉字笔画         36字    31C0-31E3
+     汉字结构         12字    2FF0-2FFB
+     汉语注音         22字    3105-3120
+     注音扩展         22字    31A0-31BA
+     〇               1字    3007
+     */
+    unsigned int number = arc4random()%(0x9FCB - 0x4E00) + 0x4E00;
+    NSData *data = [NSData dataWithBytes:&number length:2];
+    NSString *str = [[NSString alloc]initWithData:data encoding:NSUnicodeStringEncoding];
+    return str;
+}
+NSString *arc4randomEmoji(){
+    static NSArray<NSArray<NSString *> *> *emojis = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        NSString *path = [NSBundle.mainBundle pathForResource:@"emoji" ofType:@"json"];
+        NSData *data = [NSData dataWithContentsOfFile:path];
+        NSError *error = nil;
+        emojis = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingAllowFragments error:&error];
+    });
+    unsigned int idx = arc4random()%(emojis.count);
+    NSArray<NSString *> *emojiSeq = emojis[idx];
+    unsigned int number = 0;
+    for (NSInteger i = emojiSeq.count - 1; i >= 0; i--) {
+        NSScanner *scanner = [NSScanner scannerWithString:emojiSeq[i]];
+        unsigned val = 0;
+        [scanner scanHexInt:&val];
+        number |= val << (8 * i);
+    }
+    NSData *data = [NSData dataWithBytes:&number length:emojiSeq.count];
+    NSString *str = [[NSString alloc]initWithData:data encoding:NSUnicodeStringEncoding];
+    return str;
+}
